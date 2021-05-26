0d7e0a6b15f06f94b9dacd798a44a77c9a1bf0e0
Jesus Soto <sotous0@gmail.com>
2021-05-26 17:08:28 -0400

    Direct implementation of IPFS

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,11 @@
 import { EthersService } from '../ethereum/ethers.service';
 import { WatchmanRepository } from './watchman/watchman.repository';
 import { WatchmanService } from './watchman/watchman.service';
-import { IpfsStore } from '@uprtcl/ipfs-provider';
+import IPFS from 'ipfs';
 
 export const getRoutes = async () => {
-  // Connect to IPFS in order to have available a method to retrieve entities from a hash.
-  const ipfs = new IpfsStore();
+  // Create IPFS node to be able to retrieve hashes.
+  const ipfs = await IPFS.create();
   const watchmanRepo = new WatchmanRepository();
   const watchmanService = new WatchmanService(ipfs, watchmanRepo);
 
