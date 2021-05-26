9797889273c4017fe21632711eaacd00783236d0
Jesus Soto <sotous0@gmail.com>
2021-05-26 17:09:38 -0400

    Began to implement HttpStore (non functional)

--- src/services/watchman/watchman.repository.ts
+++ src/services/watchman/watchman.repository.ts
@@ -1,10 +1,22 @@
 import { EveesMutationCreate } from '@uprtcl/evees';
+import {
+  HttpMultiConnection,
+  HttpAuth0Connection,
+} from '@uprtcl/http-provider';
+import { EveesHttp, HttpStore } from '@uprtcl/evees-http';
 export class WatchmanRepository {
   constructor() {}
 
   async postNewUpdate(mutation: EveesMutationCreate): Promise<void> {
     console.log(mutation);
     console.log('Posting updates . . .');
+    const httpConnection = await new HttpSupertest(
+      process.env.HOST as string,
+      user
+    );
+
+    const httpStore = new HttpStore(httpConnection, httpCidConfig);
+    const httpEvees = new EveesHttp(httpConnection, httpStore.casID);
     // TODO: Prepare HttpStore to send data to server.
   }
 }
