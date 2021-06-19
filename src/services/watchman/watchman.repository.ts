f8833be3a1404537bb01a6266939e3bb21901f67
Jesus Soto <sotous0@gmail.com>
2021-06-19 18:36:47 -0400

    Commented out some external resources

--- src/services/watchman/watchman.repository.ts
+++ src/services/watchman/watchman.repository.ts
@@ -1,22 +1,17 @@
 import { EveesMutationCreate } from '@uprtcl/evees';
-import {
-  HttpMultiConnection,
-  HttpAuth0Connection,
-} from '@uprtcl/http-provider';
-import { EveesHttp, HttpStore } from '@uprtcl/evees-http';
 export class WatchmanRepository {
   constructor() {}
 
   async postNewUpdate(mutation: EveesMutationCreate): Promise<void> {
     console.log(mutation);
     console.log('Posting updates . . .');
-    const httpConnection = await new HttpSupertest(
-      process.env.HOST as string,
-      user
-    );
+    // const httpConnection = await new HttpSupertest(
+    //   process.env.HOST as string,
+    //   user
+    // );
 
-    const httpStore = new HttpStore(httpConnection, httpCidConfig);
-    const httpEvees = new EveesHttp(httpConnection, httpStore.casID);
+    // const httpStore = new HttpStore(httpConnection, httpCidConfig);
+    // const httpEvees = new EveesHttp(httpConnection, httpStore.casID);
     // TODO: Prepare HttpStore to send data to server.
   }
 }
