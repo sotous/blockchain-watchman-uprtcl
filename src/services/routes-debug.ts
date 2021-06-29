c64bfdafe6332611a433e8f761ffb90813a2154b
Jesus Soto <sotous0@gmail.com>
2021-06-29 17:25:30 -0400

    Created an authentication routine against our identity server.

--- src/services/routes-debug.ts
+++ src/services/routes-debug.ts
@@ -1,13 +1,25 @@
 import { EthersService } from '../ethereum/ethers.service';
+import { Auth0Connection } from '../middleware/authentication';
 import { WatchmanRepository } from './watchman/watchman.repository';
 import { WatchmanService } from './watchman/watchman.service';
 const IPFS = require('ipfs-core');
 
 export const getRoutes = async () => {
+  const auth0 = new Auth0Connection(
+    process.env.AUTH0_HOST as string,
+    process.env.CLIENT_ID as string,
+    process.env.CLIENT_SECRET as string,
+    process.env.AUTH0_AUDIENCE as string,
+    process.env.GRANT_TYPE as string
+  );
+
+  // We request microservice authentication against Auth0
+  const jwtToken = await auth0.jwtToken;
+
   // Create IPFS node to be able to retrieve hashes.
   const ipfs = await IPFS.create();
 
-  const watchmanRepo = new WatchmanRepository();
+  const watchmanRepo = new WatchmanRepository(jwtToken);
   const watchmanService = new WatchmanService(ipfs, watchmanRepo);
 
   const ethService = new EthersService(watchmanService, ipfs);
