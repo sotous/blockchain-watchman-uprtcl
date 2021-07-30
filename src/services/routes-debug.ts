38dd12cf5fe4b2ad6279696fa690ab1ce860563a
Jesus Soto <sotous0@gmail.com>
2021-07-30 15:52:01 -0500

    Remote not needed

--- src/services/routes-debug.ts
+++ src/services/routes-debug.ts
@@ -25,7 +25,7 @@ export const getRoutes = async () => {
 
   // We provide the remote.
   const watchmanRepo = new WatchmanRepository(httpRemote);
-  const watchmanService = new WatchmanService(ipfs, watchmanRepo, httpRemote);
+  const watchmanService = new WatchmanService(ipfs, watchmanRepo);
 
   const ethService = new EthersService(watchmanService, ipfs);
 
